d3f6536fbdb7de46e05a5fd707bff6bb7f270544
Ken Rutan <kenr16@gmail.com>
2017-06-02 10:32:46 -0700

    add the ability to submit new question to the site

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,19 @@
-import Ember from 'ember';
-
 export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set('addNewQuestion', true);
+    },
+
+    saveQuestion1() {
+      var params = {
+        author: this.get('author') ? this.get('author') : "",
+        date: this.get('date') ? this.get('date') : "",
+        image: this.get('image') ? this.get('image') : "",
+        question: this.get('question') ? this.get('question') : "",
+      };
+      this.set('addNewQuestion', false);
+      this.sendAction('saveQuestion2', params);
+    }
+  }
 });
